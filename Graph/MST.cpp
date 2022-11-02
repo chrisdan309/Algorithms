c896cac20bb7053a84ee6d34ba0e3f7b3b2b68bc
Christian Daniel Poma Navarro <christian.poma.n@uni.pe>
2022-11-02 04:07:30 -0500

    create undirected graph

---
+++ Graph/MST.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+using namespace std;
+
+struct arista{
+    int datoDestino;
+    int peso;
+    arista *sgteArista;
+};
+typedef arista *parista;
+
+struct vertice{
+    int datoOrigen;
+    arista *adyacente;
+    vertice *sgteVertice;
+};
+typedef vertice *pvertice;
+
+//funciones
+bool existe(parista, int);
+parista minimun(pvertice p);
+
+class grafo{
+    private:
+        pvertice pGrafo;
+    public:
+        grafo();
+        ~grafo();
+        void insertarVertice(int x);
+        void insertarArista(int x, int y, int peso);
+        void mostrarGrafo();
+        bool buscaCamino(int x, int y);
+        pvertice getPgrafo();
+        int numVertices();
+};
+
+grafo::grafo(){
+    pGrafo = NULL;
+}
+
+grafo::~grafo(){
+    pvertice p, rp;
+    parista r, ra;
+    p = pGrafo;
+    while(p!=NULL){
+        r = p->adyacente;
+        while(r!=NULL){
+            ra = r;
+            r = r->sgteArista;
+            delete ra;
+        }
+        rp = p;
+        p = p->sgteVertice;
+        delete rp;
+    }
+}
+
+pvertice grafo::getPgrafo(){
+    return pGrafo;
+}
+
+void grafo::insertarVertice(int x){
+    pvertice p;
+    p = new vertice;
+    p->datoOrigen = x;
+    p->adyacente = NULL;
+    p->sgteVertice = pGrafo;
+    pGrafo = p;
+}
+
+void grafo::insertarArista(int x, int y, int peso){
+    pvertice p;
+    parista a;
+    p = pGrafo;
+    while(p!=NULL){
+        if(p->datoOrigen == x){
+            if(!existe(p->adyacente, y)){
+                a = new arista;
+                a->datoDestino = y;
+                a->sgteArista = p->adyacente;
+                a->peso = peso;
+                p->adyacente = a;
+            }
+        }
+        p = p->sgteVertice;
+    }
+    p = pGrafo;
+    while(p!=NULL){
+        if(p->datoOrigen == y){
+            if(!existe(p->adyacente, x)){
+                a = new arista;
+                a->datoDestino = x;
+                a->sgteArista = p->adyacente;
+                a->peso = peso;
+                p->adyacente = a;
+
+            }
+        }
+        p = p->sgteVertice;
+    }
+}
+
+bool grafo::buscaCamino(int x, int y){
+    pvertice p;
+    parista a;
+    p = pGrafo;
+    while(p!=NULL){
+        if(p->datoOrigen == x){
+            a = p->adyacente;
+            while(a!=NULL){
+                if(a->datoDestino == y){
+                    return true;
+                }
+                a = a->sgteArista;
+            }
+        }
+        p = p->sgteVertice;
+    }
+    return false;
+}
+
+int grafo::numVertices(){
+    pvertice p;
+    int cont = 0;
+    p = pGrafo;
+    while(p!=NULL){
+        cont++;
+        p = p->sgteVertice;
+    }
+    return cont;
+}
+
+void grafo::mostrarGrafo(){
+    pvertice p;
+    parista a;
+    p = pGrafo;
+    while(p!=NULL){
+        cout << p->datoOrigen << " -> ";
+        a = p->adyacente;
+        while(a!=NULL){
+            cout << a->datoDestino << " ";
+            a = a->sgteArista;
+        }
+        cout << endl;
+        p = p->sgteVertice;
+    }
+}
+
+bool existe(parista a, int x){
+    while(a!=NULL){
+        if(a->datoDestino == x)
+            return true;
+        a = a->sgteArista;
+    }
+    return false;
+}
+
+parista minimun(pvertice p){
+    parista min = p->adyacente;
+    parista aux = p->adyacente;
+    while(aux!=NULL){
+        if(aux->peso < min->peso)
+            min = aux;
+        aux = aux->sgteArista;
+    }
+    return min;
+}
+
+
+bool esConexo(grafo G){
+    pvertice p = G.getPgrafo();
+    int n = G.numVertices();
+    int *v = new int[n];
+    for(int i = 0; i < n; i++){
+        v[i] = p->datoOrigen;
+        p = p->sgteVertice;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i != j){
+                if(!G.buscaCamino(v[i], v[j])){
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+grafo MST (grafo g){
+    grafo T;
+    pvertice p = g.getPgrafo();
+    pvertice t;
+    while(p!=NULL){
+        T.insertarVertice(p->datoOrigen);
+        p = p->sgteVertice;
+    }
+    p = g.getPgrafo();
+    while(p!=NULL){
+        parista a = minimun(p);
+        T.insertarArista(p->datoOrigen, a->datoDestino, a->peso);
+        p = p->sgteVertice;
+    }
+    while(!esConexo(T)){
+        p = g.getPgrafo();
+        t = T.getPgrafo();
+        
+    }
+    return T;    
+}
+
+
+
+int main(){
+    grafo G;
+    G.insertarVertice(1);
+    G.insertarVertice(2);
+    G.insertarVertice(3);
+    G.insertarVertice(4);
+    G.insertarVertice(5);
+    G.insertarVertice(6);
+    G.insertarVertice(7);
+    G.insertarVertice(8);
+    G.insertarArista(1, 2, 3);
+    G.insertarArista(1, 3, 1);
+    G.insertarArista(2, 4, 1);
+    G.insertarArista(2, 7, 5);
+    G.insertarArista(3, 4, 2);
+    G.insertarArista(3, 6, 5);
+    G.insertarArista(4, 5, 4);
+    G.insertarArista(4, 6, 2);
+    G.insertarArista(5, 7, 2);
+    G.insertarArista(5, 8, 1);
+    G.insertarArista(6, 8, 3);
+    G.mostrarGrafo();
+    grafo T = MST(G);
+    cout << "T \n";
+    T.mostrarGrafo();
+    return 0;
+}
+
+
---
+++ Graph/graph.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+
+struct arista{
+    int datoDestino;
+    arista *sgteArista;
+};
+typedef arista *parista;
+
+struct vertice{
+    int datoOrigen;
+    arista *adyacente;
+    vertice *sgteVertice;
+};
+typedef vertice *pvertice;
+
+//funciones
+bool existe(parista, int);
+
+class grafo{
+    private:
+        pvertice pGrafo;
+    public:
+        grafo();
+        ~grafo();
+        void insertarVertice(int x);
+        void insertarArista(int x, int y);
+        void mostrarGrafo();
+};
+
+grafo::grafo(){
+    pGrafo = NULL;
+}
+
+grafo::~grafo(){
+    pvertice p, rp;
+    parista r, ra;
+    p = pGrafo;
+    while(p!=NULL){
+        r = p->adyacente;
+        while(r!=NULL){
+            ra = r;
+            r = r->sgteArista;
+            delete ra;
+        }
+        rp = p;
+        p = p->sgteVertice;
+        delete rp;
+    }
+}
+
+void grafo::insertarVertice(int x){
+    pvertice p;
+    p = new vertice;
+    p->datoOrigen = x;
+    p->adyacente = NULL;
+    p->sgteVertice = pGrafo;
+    pGrafo = p;
+}
+
+void grafo::insertarArista(int x, int y){
+    pvertice p;
+    parista a;
+    p = pGrafo;
+    while(p!=NULL){
+        if(p->datoOrigen == x){
+            if(!existe(p->adyacente, y)){
+                a = new arista;
+                a->datoDestino = y;
+                a->sgteArista = p->adyacente;
+                p->adyacente = a;
+            }
+        }
+        p = p->sgteVertice;
+    }
+    p = pGrafo;
+    while(p!=NULL){
+        if(p->datoOrigen == y){
+            if(!existe(p->adyacente, x)){
+                a = new arista;
+                a->datoDestino = x;
+                a->sgteArista = p->adyacente;
+                p->adyacente = a;
+            }
+        }
+        p = p->sgteVertice;
+    }
+}
+
+void grafo::mostrarGrafo(){
+    pvertice p;
+    parista a;
+    p = pGrafo;
+    while(p!=NULL){
+        cout << p->datoOrigen << " -> ";
+        a = p->adyacente;
+        while(a!=NULL){
+            cout << a->datoDestino << " ";
+            a = a->sgteArista;
+        }
+        cout << endl;
+        p = p->sgteVertice;
+    }
+}
+
+bool existe(parista a, int x){
+    while(a!=NULL){
+        if(a->datoDestino == x)
+            return true;
+        a = a->sgteArista;
+    }
+    return false;
+}
+
+
+int main(){
+    grafo G;
+    G.insertarVertice(1);
+    G.insertarVertice(2);
+    G.insertarVertice(3);
+    G.insertarVertice(4);
+    G.insertarArista(1, 2);
+    G.insertarArista(1, 3);
+    G.insertarArista(2, 3);
+    G.insertarArista(2, 4);
+    G.mostrarGrafo();
+    return 0;
+}
+
